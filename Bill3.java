dee2f4d9846eacf4820abf1960cf9e7d22f819e3
talkz199 <156941581+talkz199@users.noreply.github.com>
2025-11-06 21:09:12 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,13 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+
+		int bill = Integer.parseInt(args[3]);
+		bill = bill/3;
+	   System.out.println("Dear " + name1 + ", " + name2 + "and " + name3 + "pay " + bill + " Shekels each.");
 	}
+	
 }
+
