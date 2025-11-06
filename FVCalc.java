893041b473b87a5b97e15c9bab73564c8ed0e459
talkz199 <156941581+talkz199@users.noreply.github.com>
2025-11-06 21:08:18 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,15 @@
-// Computes the future value of a saving investment.
-public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+// Splits a restaurant bill evenly among three diners.
+public class Bill3 {
+	public static void main(String[] args) {
+		// To get you started, here is the first line in the program:
+	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+
+		int bill = Integer.parseInt(args[3]);
+		bill = bill/3;
+	   System.out.println("Dear " + name1 + ", " + name2 + "and " + name3 + "pay " + bill + " Shekels each.");
 	}
+	
 }
+
